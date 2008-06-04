f71c470583307975e22ceb4e8ee1721ac2c670e3
piumarta <piumarta@c02a89d2-2f2a-0410-ba84-dc1a35339576>
2008-06-04 18:56:55 +0000

    add win32 support to examples2/tcp

---
+++ function/examples2/tcp/win32.h
@@ -0,0 +1,13 @@
+#ifndef __NPIwin32_h
+#define __NPIwin32_h
+
+#define WINVER 0x0501
+
+#include <windows.h>
+#include <winioctl.h>
+#include <ws2tcpip.h>
+
+#include <stdio.h>
+#include <stdint.h>
+
+#endif /* __NPIwin32_h */
