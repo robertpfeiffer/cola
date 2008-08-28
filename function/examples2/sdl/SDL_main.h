e9fccb73676a035df39dcebebbb111bf03378c02
piumarta <piumarta@c02a89d2-2f2a-0410-ba84-dc1a35339576>
2008-08-28 16:16:59 +0000

    treat SDL_main in a more portable way

--- function/examples2/sdl/SDL_main.h
+++ function/examples2/sdl/SDL_main.h
@@ -1,3 +1,11 @@
-#if defined(MACH)
-# define main SDL_main
-#endif
+#include "SDL/SDL.h"
+
+int __id__SDL_main(int argc, char *argv[], char **envp);
+
+int main(int argc, char *argv[])
+{
+  return __id__SDL_main(argc, argv, 0);
+}
+
+#undef main
+#define main __id__SDL_main
