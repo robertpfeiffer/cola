c83bce0eb4590ea976e4b6dccf2426ec8e46988c
piumarta <piumarta@c02a89d2-2f2a-0410-ba84-dc1a35339576>
2008-09-02 17:58:59 +0000

    statistical profiling is synchronous at method entry/exit

--- function/objects/sigalrm.h
+++ function/objects/sigalrm.h
@@ -1,7 +1,53 @@
 static oop sigalrmReceiver= 0;
 static oop sigalrmSelector= 0;
+static int statisticalSampler= 0;
+
+static void *(*enter)(struct __methodinfo *info);
+static void  (*leave)(void *cookie);
+
+static void  enableStatisticalProfile(void);
+static void  disableStatisticalProfile(void);
+static void *statisticalProfileEnter(struct __methodinfo *info);
+static void  statisticalProfileLeave(void *cookie);
+
+static void *statisticalProfileEnter(struct __methodinfo *info)
+{
+  if (statisticalSampler)
+    {
+      statisticalSampler= 0;
+      _sendv(sigalrmSelector, 1, sigalrmReceiver);
+    }
+  return enter(info);
+  (void)statisticalProfileEnter;
+}
+
+static void  statisticalProfileLeave(void *cookie)
+{
+  leave(cookie);
+  (void)statisticalProfileLeave;
+}
+
+static void enableStatisticalProfile(void)
+{
+  if (enter != statisticalProfileEnter)
+    {
+      enter= _libid->enter;  _libid->enter= statisticalProfileEnter;
+      leave= _libid->leave;  _libid->leave= statisticalProfileLeave;
+    }
+  (void)enableStatisticalProfile;
+}
+
+static void disableStatisticalProfile(void)
+{
+  if (enter == statisticalProfileEnter)
+    {
+      _libid->enter= enter;  enter= 0;
+      _libid->leave= leave;  leave= 0;
+    }
+  (void)disableStatisticalProfile;
+}
 
 static void sigalrm(int sig)
 {
-  _sendv(sigalrmSelector, 1, sigalrmReceiver);
+  ++statisticalSampler;
 }
